1a5de91f5348388eed82b9e63898631e263c4151
Creator <382988057@qq.com>
2022-04-10 19:10:16 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-03-27 14:21:01
- * @LastEditTime: 2022-04-10 19:07:11
+ * @LastEditTime: 2022-04-10 19:10:09
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue-front/src/App.vue
@@ -28,7 +28,7 @@ export default {
     }
   },
   mounted() {
-    axios.get('http://112.74.92.175/:3000/api/users').then((res) => {
+    axios.get('http://112.74.92.175:3000/api/users').then((res) => {
       console.log(res, 'dd')
       this.users = res.data
     })
