b2a1c5c532ac190b920fc4cdbb51eff4b9f5825c
Creator <382988057@qq.com>
2022-04-10 18:15:44 +0800

    dds

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-03-27 14:21:01
- * @LastEditTime: 2022-03-27 14:28:36
+ * @LastEditTime: 2022-04-10 18:15:32
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue-front/src/App.vue
@@ -9,6 +9,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <div>哈哈哈</div>
     <ul>
       <li v-for="item in users" :key="item.id">
         {{ item.name }}
